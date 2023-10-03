71fcb63a49297780da07934a7120bd528eced4ff
Wellington Ozorio <Wellington.Ozorio@br.bosch.com>
2023-10-03 15:43:03 -0300

    Update

--- __main__.py
+++ __main__.py
@@ -34,7 +34,7 @@ def main():
         resource_group.name,
         subnet_id=subnet.id,
         private_ip_address=private_ip_address,
-        public_ip_address=public_ip.id,
+        public_ip_address_id=public_ip.id,
         tags=TAGS,
     )
 
@@ -115,7 +115,9 @@ def create_network_interface(name, resource_group_name, **kwargs):
                 subnet=network.SubnetArgs(id=kwargs.get("subnet_id")),
                 private_ip_allocation_method=kwargs.get("private_ip_allocation_method", "Static"),
                 private_ip_address=kwargs.get("private_ip_address"),
-                public_ip_address=kwargs.get("public_ip_address"),
+                public_ip_address=network.PublicIPAddressArgs(
+                    id=kwargs.get("public_ip_address_id"),
+                ),
             )
         ],
         tags=kwargs.get("tags"),
