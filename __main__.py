7b342cc86eec4541813f71d7027c7d7faa905ccd
Wellington Ozorio <well.ozorio@gmail.com>
2023-10-03 12:28:00 -0300

    Update

--- __main__.py
+++ __main__.py
@@ -1,83 +1,169 @@
 """An Azure RM Python Pulumi program"""
 
-import nsg
 import pulumi
-import vm
 from pulumi_azure_native import network, resources
 
+import vm
+
+TAGS = {"created_by": "pulumi"}
 
 def main():
     """The main function."""
     config = pulumi.Config()
+    admin_username = config.get("admin_username")
+    private_ip_address = config.get("private_ip_address")
+    virtual_network_address_space = config.get("virtual_network_address_space")
+    subnet_address_space = config.get("subnet_address_space")
 
-    admin_username = config.get("adminUsername")
-    ssh_public_key = config.get("sshPublicKey")
+    resource_group = resources.ResourceGroup("playground-wozorio", tags=TAGS)
 
-    tags = {"created_by": "pulumi"}
-
-    resource_group = resources.ResourceGroup("playground", tags=tags)
-
-    virtual_network = create_virtual_network(resource_group.name, resource_group.location)
-    subnet = create_subnet(resource_group.name, virtual_network.name)
-
-    network_security_group = nsg.create_network_security_group(resource_group.name)
-    nsg.create_nsg_rule(
-        network_security_group.resource_group_name,
-        network_security_group.name,
-        "Rule1",
-        priority=100,
-        protocol="Tcp",
-        destination_port_range="80",
-        source_address_prefix="*",
-    )
-    nsg.create_nsg_rule(
-        network_security_group.resource_group_name,
-        network_security_group.name,
-        "Rule2",
-        priority=200,
-        protocol="Tcp",
-        destination_port_range="22",
-        source_address_prefix="*",
+    virtual_network = create_virtual_network(
+        "vnet",
+        resource_group.name,
+        resource_group.location,
+        virtual_network_address_space=virtual_network_address_space,
+        tags=TAGS
     )
 
-    public_ip = create_public_ip(resource_group.name, resource_group.location)
+    subnet = create_subnet("snet", resource_group.name, virtual_network.name, subnet_address_space=subnet_address_space)
 
-    vm.create_vm(resource_group.name, resource_group.location, admin_username, ssh_public_key, subnet.id)
+    network_interface=create_network_interface(
+        "nic_ubuntu",
+        resource_group.name,
+        subnet_id=subnet.id,
+        private_ip_address=private_ip_address,
+        tags=TAGS
+    )
 
-    pulumi.export("publicIP", public_ip.ip_address)
+    network_security_group = create_network_security_group("nsg", resource_group.name, resource_group.location, tags=TAGS)
+    pulumi.Output.all(resource_group.name, network_security_group.name).apply(
+        lambda args: create_nsg_rule(
+            "Allow-HTTP-From-Internet-To-VM",
+            args[0],
+            args[1],
+            priority=100,
+            protocol="Tcp",
+            source_port_range="*",
+            destination_port_range="80",
+            source_address_prefix="Internet",
+            destination_address_prefix = f"{private_ip_address}/32"
+        )
+    )
+    pulumi.Output.all(resource_group.name, network_security_group.name).apply(
+        lambda args: create_nsg_rule(
+            "Allow-SSH-From-Internet-To-VM",
+            args[0],
+            args[1],
+            priority=200,
+            protocol="Tcp",
+            source_port_range="*",
+            destination_port_range="22",
+            source_address_prefix="Internet",
+            destination_address_prefix = f"{private_ip_address}/32"
+        )
+    )
 
+    public_ip = create_public_ip("pip-ubuntu", resource_group.name, resource_group.location, tags=TAGS)
 
-def create_virtual_network(resource_group_name, location):
+    vm.create_vm(
+        "ubuntu",
+        resource_group.name,
+        resource_group.location,
+        admin_username=admin_username,
+        network_interface_id=network_interface.id,
+        public_ip_address=public_ip.id,
+        subnet_id=subnet.id,
+        tags=TAGS
+    )
+
+    pulumi.export("Ubuntu VM public IP", public_ip.ip_address)
+
+def create_virtual_network(name, resource_group_name, location, **kwargs):
     """Create a virtual network."""
     virtual_network = network.VirtualNetwork(
-        "serverNetwork",
+        name,
         resource_group_name=resource_group_name,
         location=location,
         address_space=network.AddressSpaceArgs(
-            address_prefixes=["10.0.0.0/16"],
+            address_prefixes=[kwargs.get("virtual_network_address_space")],
         ),
+        tags=kwargs.get("tags")
     )
     return virtual_network
 
 
-def create_subnet(resource_group_name, virtual_network_name):
+def create_subnet(name, resource_group_name, virtual_network_name, **kwargs):
     """Create a subnet."""
     subnet = network.Subnet(
-        "serverSubnet",
+        name,
         resource_group_name=resource_group_name,
         virtual_network_name=virtual_network_name,
-        address_prefix="10.0.1.0/24",
+        address_prefix=kwargs.get("subnet_address_space"),
     )
     return subnet
 
+def create_network_interface(name, resource_group_name, **kwargs):
+    """Create a network interface."""
+    network_interface = network.NetworkInterface(
+        name,
+        resource_group_name=resource_group_name,
+        ip_configurations=[
+            network.NetworkInterfaceIPConfigurationArgs(
+                name="ipconfig",
+                subnet=network.SubnetArgs(id=kwargs.get("subnet_id")),
+                private_ip_allocation_method=kwargs.get("private_ip_allocation_method", "Static"),
+                private_ip_address=kwargs.get("private_ip_address"),
+                public_ip_address=kwargs.get("public_ip_address"),
+            )
+        ],
+        tags=kwargs.get("tags")
+    )
+    return network_interface
 
-def create_public_ip(resource_group_name, location):
+def create_network_security_group(name, resource_group_name, location, **kwargs):
+    """Create a network security group."""
+    nsg = network.NetworkSecurityGroup(
+        name,
+        resource_group_name=resource_group_name,
+        location=location,
+        tags=kwargs.get("tags")
+    )
+    return nsg
+
+def create_nsg_rule(name, resource_group_name, network_security_group_name, **kwargs) -> network.SecurityRule:
+    """Create a NSG rule."""
+    nsg_rule = network.SecurityRule(
+        name,
+        resource_group_name=resource_group_name,
+        network_security_group_name=network_security_group_name,
+        protocol=kwargs.get("protocol", "Tcp"),
+        source_port_range=kwargs.get("source_port_range"),
+        destination_port_range=kwargs.get("destination_port_range"),
+        source_address_prefix=kwargs.get("source_address_prefix"),
+        destination_address_prefix=kwargs.get("destination_address_prefix"),
+        access=kwargs.get("access", "Allow"),
+        direction=kwargs.get("direction", "Inbound"),
+        priority=kwargs.get("priority")
+    )
+    return nsg_rule
+
+def create_public_ip(name, resource_group_name, location, **kwargs):
     """Create a public IP."""
     public_ip = network.PublicIPAddress(
-        "serverPublicIp", resource_group_name=resource_group_name, location=location, public_ip_allocation_method="Static"
+        name,
+        resource_group_name=resource_group_name,
+        location=location,
+        public_ip_allocation_method="Dynamic",
+        sku=network.PublicIPAddressSkuArgs(
+            name="Basic"
+        ),
+        dns_settings=network.PublicIPAddressDnsSettingsArgs(
+            domain_name_label="vm-ubuntu9"
+        ),
+        public_ip_address_version="IPv4",
+        tags=kwargs.get("tags")
     )
     return public_ip
-
 
 if __name__ == "__main__":
     main()
--- vm.py
+++ vm.py
@@ -1,21 +1,24 @@
+"""Deploy a virtual machine."""
 from pulumi_azure_native import compute
 
 
-def create_vm(resource_group_name, location, admin_username, ssh_public_key, network_interface_id):
+def create_vm(name, resource_group_name, location, **kwargs):
     """Create a Linux VM."""
+    ssh_public_key=get_ssh_public_key("id_rsa.pub")
+
     compute.VirtualMachine(
-        "serverVM",
+        name,
         resource_group_name=resource_group_name,
         location=location,
         os_profile=compute.OSProfileArgs(
-            admin_username=admin_username,
-            computer_name="hostname",
+            admin_username=kwargs.get("admin_username"),
+            computer_name=name,
             linux_configuration=compute.LinuxConfigurationArgs(
                 disable_password_authentication=True,
                 ssh=compute.SshConfigurationArgs(
                     public_keys=[
                         compute.SshPublicKeyArgs(
-                            path="/home/username/.ssh/authorized_keys",
+                            path=(f"/home/{kwargs.get('admin_username')}/.ssh/authorized_keys"),
                             key_data=ssh_public_key,
                         )
                     ],
@@ -25,19 +28,25 @@ def create_vm(resource_group_name, location, admin_username, ssh_public_key, net
         network_profile=compute.NetworkProfileArgs(
             network_interfaces=[
                 compute.NetworkInterfaceReferenceArgs(
-                    id=network_interface_id,
+                    id=kwargs.get("network_interface_id"),
                 ),
             ],
         ),
         hardware_profile=compute.HardwareProfileArgs(
-            vm_size="Standard_A0",
+            vm_size="Standard_B2s",
         ),
         storage_profile=compute.StorageProfileArgs(
             image_reference=compute.ImageReferenceArgs(
-                publisher="canonical",
-                offer="UbuntuServer",
-                sku="22_04-lts",
-                version="latest",
+                publisher=kwargs.get("publisher", "canonical"),
+                offer=kwargs.get("offer", "0001-com-ubuntu-server-jammy"),
+                sku=kwargs.get("sku", "22_04-lts-gen2"),
+                version=kwargs.get("version","latest")
             ),
         ),
+        tags=kwargs.get("tags")
     )
+
+def get_ssh_public_key(file_path) -> str:
+    """Read the SSH public key from a file."""
+    with open(file_path, 'r', encoding="utf-8") as file:
+        return file.read().replace('\n', '')
