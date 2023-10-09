2248c87546fe289dcc58d17bd7e3c4386cc74665
Wellington Ozorio <Wellington.Ozorio@br.bosch.com>
2023-10-09 16:43:15 -0300

    Associate network interface with NSG

--- __main__.py
+++ __main__.py
@@ -76,7 +76,7 @@ def main() -> None:
         tags=TAGS,
     )
 
-    associate_network_interace_with_nsg(network_interface.id, network_security_group.id)
+    associate_network_interface_with_nsg(network_interface.id, network_security_group.id)
 
     pulumi.export("Virtual machine FQDN", public_ip.dns_settings.apply(lambda dns: dns.fqdn))
 
@@ -167,7 +167,7 @@ def create_nsg_rule(name: str, resource_group_name: str, network_security_group_
     )
 
 
-def associate_network_interace_with_nsg(network_interface_id: str, network_security_group_id: str) -> None:
+def associate_network_interface_with_nsg(network_interface_id: str, network_security_group_id: str) -> None:
     """Associate a network interface with a NSG."""
     azure.network.NetworkInterfaceSecurityGroupAssociation(network_interface_id, network_security_group_id)
 
