8eebd98b3b768b048f768e9d472cd3a71afa1055
Wellington Ozorio <well.ozorio@gmail.com>
2023-10-10 16:50:38 -0300

    Update

--- __main__.py
+++ __main__.py
@@ -18,7 +18,7 @@ class NSGRule:
     access: str
     protocol: str
     source_port_range: str
-    destination_port_range: int
+    destination_port_range: str
     source_address_prefix: str
     destination_address_prefix: str
 
